86a281cfcbfc95a534d415a65e268442ce216dac
gulian <gulian@gulian.fr>
2018-09-12 15:21:26 +0200

    general: initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
 
-function getParameterByName(name, url) {
+exports.getParameterByName = (name, url) => {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -9,5 +9,3 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
-
-export default getParameterByName;
